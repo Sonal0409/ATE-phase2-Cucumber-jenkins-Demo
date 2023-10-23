1f1c855d2c9d9f1d798584e9f029c67db4d5feaa
sonal0409 <mittal.sonal04@gmail.com>
2023-10-23 09:29:21 +0530

    done

---
+++ test-ouput-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"52c36a83-4074-4e95-8200-daa1a2396173","feature":"Test the Wiki page on chrome Browser","scenario":"Test Login link on Wiki Page","start":1697787113722,"group":1,"content":"","tags":"@all,@login,@functional,","end":1697787130392,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
